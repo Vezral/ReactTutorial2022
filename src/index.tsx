edc334f263e0a2fd0f750254eec6ea26ade605e5
Vezral <kimseangtancs@gmail.com>
2022-10-25 07:45:49 +0800

    added docker compose with different envs

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import "./index.scss";
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
+    <p>App Test Variable: {process.env.REACT_APP_TEST_VARIABLE}</p>
+    <p>Node Env: {process.env.NODE_ENV}</p>
     <Game />
   </React.StrictMode>
 );
